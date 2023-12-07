1498fd401fa7d7b8a244c4c4361af7291f37d2df
Arin Ghazarian <aringhazarian@github.com>
2023-12-07 14:36:55 -0800

    Day 2 part 2

--- day-02/Program.cs
+++ day-02/Program.cs
@@ -1,17 +1,17 @@
 ﻿var games = File.ReadAllLines("./input.txt");
 
-var fact = (Red: 12, Green: 13, Blue: 14);
-
 long sum = 0;
 foreach (var line in games)
 {
-    var gameId = int.Parse(line[..line.IndexOf(':')].Replace("Game ", ""));
-    var sets = line[(line.IndexOf(':') + 1)..].Split(';')
-        .Select(set => ConvertToRgbValues(set.Split(',')));
-    if (IsPossibleGame(sets))
-    {
-        sum += gameId;
-    }
+    var sets = line[(line.IndexOf(':') + 1)..].Split(';').Select(set => ConvertToRgbValues(set.Split(',')));
+    var power = sets.Aggregate(
+        seed: (Red: 0, Green: 0, Blue: 0),
+        (previous, current) => (
+            Red: Math.Max(previous.Red, current.Red),
+            Green: Math.Max(previous.Green, current.Green),
+            Blue: Math.Max(previous.Blue, current.Blue)),
+        set => set.Red * set.Green * set.Blue);
+    sum += power;
 }
 
 Console.WriteLine(sum);
@@ -25,12 +25,9 @@ Console.WriteLine(sum);
         var combo = value.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
         var count = int.Parse(combo[0].Trim());
         var color = combo[1].Trim();
-        rgbCount[color] += count;
+        rgbCount[color] = Math.Max(rgbCount[color], count);
     }
 
     return (Red: rgbCount["red"], Green: rgbCount["green"], Blue: rgbCount["blue"]);
 }
-
-bool IsPossibleGame(IEnumerable<(int Red, int Green, int Blue)> sets) =>
-    sets.All(set => set.Red <= fact.Red && set.Green <= fact.Green && set.Blue <= fact.Blue);
 
