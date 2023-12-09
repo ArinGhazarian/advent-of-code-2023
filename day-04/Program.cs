3e622c4bf5acb27929af6432d00b01c42988ec97
Arin Ghazarian <aringhazarian@github.com>
2023-12-09 13:07:23 -0800

    Day 4 part 1

--- day-04/Program.cs
+++ day-04/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = File.ReadAllLines("./input.txt");
+
+var totalPoints = 0L;
+foreach (var line in input)
+{
+    var (_, winningNumbers, yourNumbers) = ParseCardInfo(line);
+    var count = yourNumbers.Intersect(winningNumbers).Count();
+    var points = (long)Math.Pow(2, count - 1);
+    totalPoints += points;
+}
+
+Console.WriteLine(totalPoints);
+
+
+(int Card, int[] WinningNumbers, int[] YourNumbers) ParseCardInfo(string line)
+{
+    var colonIndex = line.IndexOf(':');
+    var pipeIndex = line.IndexOf('|');
+    
+    var card = int.Parse(line[..colonIndex].Replace("Card", "").Trim());
+    var winningNumbers = line[(colonIndex + 1)..pipeIndex]
+        .Trim()
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(n => int.Parse(n))
+        .ToArray();
+    var yourNumbers = line[(pipeIndex + 1)..]
+        .Trim()
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(n => int.Parse(n))
+        .ToArray();
+
+    return (card, winningNumbers, yourNumbers);
+}
