3d75cb0abc11c1365fcb55e311315e63ff00e9e9
Arin Ghazarian <aringhazarian@github.com>
2023-12-09 17:50:57 -0800

    Day 4 part 2

--- day-04/Program.cs
+++ day-04/Program.cs
@@ -1,22 +1,26 @@
 ﻿var input = File.ReadAllLines("./input.txt");
 
-var totalPoints = 0L;
+var cards = new Dictionary<int, int>(Enumerable.Range(1, input.Length).Select(i => new KeyValuePair<int, int>(i, 1)));
 foreach (var line in input)
 {
-    var (_, winningNumbers, yourNumbers) = ParseCardInfo(line);
-    var count = yourNumbers.Intersect(winningNumbers).Count();
-    var points = (long)Math.Pow(2, count - 1);
-    totalPoints += points;
+    var (cardNumber, winningNumbers, yourNumbers) = ParseCardInfo(line);
+    var cardsWon = yourNumbers.Intersect(winningNumbers).Count();
+    var count = cards[cardNumber];
+    for (var i = 1; i <= cardsWon; i++)
+    {
+        cards[cardNumber + i] += count;
+    }
 }
 
-Console.WriteLine(totalPoints);
+var totalCards = cards.Values.Sum(x => (long)x);
+Console.WriteLine(totalCards);
 
 
 (int Card, int[] WinningNumbers, int[] YourNumbers) ParseCardInfo(string line)
 {
     var colonIndex = line.IndexOf(':');
     var pipeIndex = line.IndexOf('|');
-    
+
     var card = int.Parse(line[..colonIndex].Replace("Card", "").Trim());
     var winningNumbers = line[(colonIndex + 1)..pipeIndex]
         .Trim()
